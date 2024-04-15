73e8c62e2fdcb781f4f810c6cce339674815580c
amitg <amitg@Amit>
2024-04-15 13:48:55 +0530

    Test Class

---
+++ Test/src/com/test/Ttest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.test;
+
+/**
+ * 
+ */
+public class Ttest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
